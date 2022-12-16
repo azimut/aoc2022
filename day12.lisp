199bb35a1d11329b7ad77b2f8ae8e186003b739a
azimut <azimut.github@protonmail.com>
2022-12-16 15:44:57 -0300

    aoc day 12 gold

--- day12.lisp
+++ day12.lisp
@@ -7,14 +7,11 @@
 
 (in-package #:aoc2022-day12)
 
-(defstruct cell row-idx cost)
-
 (defun slurp (f) (a:read-file-into-string f))
-(defun xrid (cols pos) (list (floor (/ pos cols)) (- pos (* (floor (/ pos cols)) cols))))
 
 (defun make-adj-list (mat &aux (rows (array-dimension mat 0)) (cols (array-dimension mat 1)))
   (flet ((f (x y cols) (+ y (* x cols))))
-    (s:lret ((adj (make-hash-table :test #'equal)))
+    (s:lret ((adj (make-hash-table)))
       (array-operations/utilities:nested-loop (x y) (array-dimensions mat)
         (when (> x 0)         (push (f (1- x) y cols) (s:href adj (f x y cols))))
         (when (> y 0)         (push (f x (1- y) cols) (s:href adj (f x y cols))))
@@ -22,11 +19,11 @@
         (when (< y (1- cols)) (push (f x (1+ y) cols) (s:href adj (f x y cols))))))))
 
 (defun make-heightmap (asciimap)
-  (flet ((f (c) 
-           (- (char-code (case c (#\S #\a) (#\E #\z) (t c)))
-              (char-code #\a))))
-    (aops:each-index (x y)
-      (f (aref asciimap x y)))))
+  (s:lret ((result (make-array (array-dimensions asciimap) :element-type 'fixnum)))
+    (flet ((f (c) (- (char-code (case c (#\S #\a) (#\E #\z) (t c)))
+                     (char-code #\a))))
+      (array-operations/utilities:nested-loop (x y) (array-dimensions asciimap)
+        (setf (aref result x y) (f (aref asciimap x y)))))))
 
 (defun adj-lists (filename &aux (raw-arr (slurp filename)))
   (let* ((start (~>> raw-arr (remove #\NewLine) (position #\S)))
@@ -41,16 +38,41 @@
 
 ;;------------------------------
 
-(defun make-inf-array (n) (make-array n :initial-element MOST-POSITIVE-FIXNUM))
-
-(defun bfs (start end asciimap heightmap adjlist &aux (distances (make-inf-array (array-total-size asciimap))) (queue (s:queue)))
+(defun bfs (start end heightmap adjlist
+            &aux (size (array-total-size heightmap))
+              (visited (make-array size :initial-element NIL))
+              (prev  (make-array size  :initial-element -1))
+              (queue (s:queue)))
   (s:enq start queue)
-  (setf (aref distances start) 0)
-  (dolist (i (s:href adjlist start)) (s:enq i queue))
-  (loop :for idx := (s:deq queue)
-        :while idx
-        :do ))
+  (loop :until (s:queue-empty-p queue)
+        :for idx := (s:deq queue)
+        :do (dolist (next (s:href adjlist idx))
+              (when (and (not (aref visited next))
+                         (<= (row-major-aref heightmap next)
+                             (1+ (row-major-aref heightmap idx))))
+                (setf (aref visited next) T)
+                (s:enq next queue)
+                (setf (aref prev next) idx))))
+  (length 
+   (bfs-reconstruct prev start end)))
+
+(defun bfs-reconstruct (prev start end)
+  (loop :with at := end
+        :until (or (minusp at) (= at start))
+        :collect (setf at (aref prev at))))
 
 (defun silver (filename)
-  (destructuring-bind (&key adj start end mat &allow-other-keys) (adj-lists filename)
-    (bfs start end (array-dimension mat 1) adj)))
+  (destructuring-bind (&key adj start end heightmap asciimap) (adj-lists filename)
+    (bfs start end heightmap adj)))
+
+(defun find-starts (heightmap)
+  (loop :for idx :below (array-total-size heightmap)
+        :when (zerop (row-major-aref heightmap idx))
+          :collect idx))
+
+(defun gold (filename)
+  (destructuring-bind (&key adj end heightmap &allow-other-keys) (adj-lists filename)
+    (loop :for some-a :in (find-starts heightmap)
+          :for distance := (bfs some-a end heightmap adj)
+          :when (> distance 1)
+            :minimizing distance)))
