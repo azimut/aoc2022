96b6a862ebe970072fa86a9d1b42bd29e508daa0
azimut <azimut.github@protonmail.com>
2022-12-22 23:49:00 -0300

    aoc 21 still not working...

--- day21.ml
+++ day21.ml
@@ -3,17 +3,17 @@ open! Batteries
 (* PART 2 = 301 = *)
 type message  =
   | Number of int
-  | Op of string * string * (int->int->int)
+  | Op of string * string * (int->int->int) * (int->int->int) * string
 
 type monkey = { id: string; message: message }
 
 let make_monkey (id,raw_message) =
   let message = match String.(trim raw_message |> split_on_char ' ') with
     | [x]       -> Number (Int.of_string x)
-    | [x;"*";y] -> Op (x, y, Int.mul)
-    | [x;"/";y] -> Op (x, y, Int.div)
-    | [x;"+";y] -> Op (x, y, Int.add)
-    | [x;"-";y] -> Op (x, y, Int.sub)
+    | [x;"*";y] -> Op (x, y, Int.mul, Int.div, "*")
+    | [x;"/";y] -> Op (x, y, Int.div, Int.mul, "/")
+    | [x;"+";y] -> Op (x, y, Int.add, Int.sub, "+")
+    | [x;"-";y] -> Op (x, y, Int.sub, Int.add, "-")
     | _         -> assert false
   in
   {id;message}
@@ -33,7 +33,7 @@ let silver monkeys =
     | {id;message=Number(value)} :: ms ->
        add monkeys_hash id value;
        loop ms
-    | {id;message=Op(id1,id2,f)} :: ms when mem monkeys_hash id1 && mem monkeys_hash id2 ->
+    | {id;message=Op(id1,id2,f,_,_)} :: ms when mem monkeys_hash id1 && mem monkeys_hash id2 ->
        add monkeys_hash id (f (find monkeys_hash id1) (find monkeys_hash id2));
        loop ms
     | {id;message} :: ms ->
@@ -44,26 +44,27 @@ let silver monkeys =
 
 let root_deps monkeys =
   match List.find (fun m -> m.id = "root") monkeys with
-    | {id=_;message=Op(left,right,_)} -> left,right
+    | {id=_;message=Op(left,right,_,_,_)} -> left,right
     | _                               -> assert false
 
-let rec solve_non_humn_opt monkeys search_id =
+let rec solve_root_deps_opt monkeys search_id =
   let next_monkey = List.find (fun m -> m.id = search_id) monkeys in
   match next_monkey with
   | {id=_;message=Number(number)} -> Some number
-  | {id=_;message=Op(i1,i2,_)} when i2 = "humn" || i1 = "humn" -> None
-  | {id=_;message=Op(i1,i2,f)}    ->
-     Option.bind (solve_non_humn_opt monkeys i1) (fun m1 ->
-       Option.bind (solve_non_humn_opt monkeys i2) (fun m2 ->
+  | {id=_;message=Op(i1,i2,_,_,_)} when i2 = "humn" || i1 = "humn" -> None
+  | {id=_;message=Op(i1,i2,f,_,_)} ->
+     Option.bind (solve_root_deps_opt monkeys i1) (fun m1 ->
+       Option.bind (solve_root_deps_opt monkeys i2) (fun m2 ->
            Some(f m1 m2)))
 
+let solve monkeys total from_id =
+  let from_monkey = List.find (fun monkey -> monkey.id = from_id) monkeys in
+  match from_monkey with
+  | {id=_;message=Number(n)}           -> n
+  | {id=_;message=Op(id1,id2,_,f,"-")} -> f total
+  | {id=_;message=Op(id1,id2,_,f,_)}   -> f total
+
 let gold monkeys =
-  let dep1,dep2 = root_deps monkeys in
-  let sum1, sum2 = solve_non_humn_opt monkeys dep1, solve_non_humn_opt monkeys dep2 in
-  let found = ref false in
-  let tmp = ref 0 in
-  while !found do
-    incr tmp;
-    found := true
-  done;
-  Printf.printf "%s=%d - %s=%d\n" dep1 (Option.default 0 sum1) dep2 (Option.default 0 sum2)
+  let left,right = root_deps monkeys in
+  let left_sum, right_sum = solve_root_deps_opt monkeys left, solve_root_deps_opt monkeys right in
+  Printf.printf "%s=%d - %s=%d\n" left (Option.default 0 left_sum) right (Option.default 0 right_sum)
